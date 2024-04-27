bbc0e0a6e633d21e112a28010c13d54759d4fb0a
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2024-04-27 12:32:44 -0300

    Agregada cámara cinemachine y configuraciones
    Se añadió la clase CameraObjective, que se añade a un objeto que seguirá al jugador. Este objeto responde al input de movimiento de cámara del jugador y la Virtual Camera lo sigue. De esta forma, logramos un control y cámara más libres.

---
+++ Assets/Camera/CameraObjective.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraObjective : MonoBehaviour
+{
+    [Header("Seguimiento al jugador")]
+    [SerializeField]
+    [Range(0f, 0.3f)]float suavizadoMovimiento;
+    [Header("Cámara")]
+    [SerializeField]
+    float sensibilidadX;
+    [SerializeField]
+    float sensibilidadY;
+    [SerializeField]
+    [Range(1, 10)] float sentivityProportion;
+    [SerializeField]
+    [Range(0f, 0.3f)] float smoothing;
+    [SerializeField]
+    float lowerLookLimit;
+    [SerializeField]
+    float upperLookLimit;
+
+    Vector2 input_vector = Vector2.zero;
+    float rotacionEjeX;
+    Vector2 suavidadV;
+
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        rotacionEjeX = transform.localEulerAngles.x;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.transform.position, (1f / suavizadoMovimiento) * Time.deltaTime);
+        rotarCamara();
+    }
+
+    void rotarCamara()
+    {
+        if (Time.timeScale == 0)
+        {
+            return;
+        }
+
+        //movimiento = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        suavidadV.x = Mathf.SmoothStep(suavidadV.x, input_vector.x * sensibilidadX * 1 / sentivityProportion, smoothing);
+        suavidadV.y = Mathf.SmoothStep(suavidadV.y, input_vector.y * sensibilidadY * 1 / sentivityProportion, smoothing);
+
+
+        // Rotamos verticalmente la cámara
+        // 1 - Obtenemos la rotación objetivo de la cámara
+        rotacionEjeX += suavidadV.y;
+
+        // 2- Limitamos la rotación total a 90 grados
+        rotacionEjeX = Mathf.Clamp(rotacionEjeX, lowerLookLimit, upperLookLimit);
+
+        // 3- Rotamos en valores locales
+        transform.localRotation = Quaternion.Euler(-rotacionEjeX, suavidadV.x + transform.localEulerAngles.y, 0);
+    }
+    void OnRotate(InputValue inputValue)
+    {
+        input_vector = inputValue.Get<Vector2>();
+    }
+}
