8dfb80ae78d9f33767c58d9af549b2fe7324a728
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2024-04-27 17:51:45 -0300

    El proyectil explosivo ahora añade fuerza a los rigidbody con los que colisiona. Configurados Layers para que el proyectil no explote al colisionar con el jugador.

--- Assets/Weapon/Scripts/Proyectile.cs
+++ Assets/Weapon/Scripts/Proyectile.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class Proyectile : MonoBehaviour
 {
     [SerializeField] int damage;
+    [SerializeField] float explosionForce;
     [SerializeField] float radiusOfExplosion;
     private void OnCollisionEnter(Collision collision)
     {
         Collider[] collisions = Physics.OverlapSphere(transform.position, radiusOfExplosion);
         foreach(Collider col in collisions)
         {
+            Rigidbody rb = col.GetComponent<Rigidbody>();
             IDamageable damageable = col.GetComponent<IDamageable>();
             if (damageable != null)
             {
                 damageable.Damage(damage);
+            }
+            if(rb != null)
+            {
+                rb.AddExplosionForce(explosionForce, transform.position, radiusOfExplosion);
             }
         }
         Destroy(gameObject);
